ea515d00ea8ce7059756ce5df42f40e6c3af827e
Richard Fenix <rfenix@Richards-MBP.attlocal.net>
2019-07-05 20:18:05 -0400

    First commit.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+		<div className="ui container grid">
+			<div className="ui row">App
+			</div>
+    </div>
+  )
+}
+
+export default App;
