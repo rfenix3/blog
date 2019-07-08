0debd4d400cbd424fc1286346f246f0655628d5a
Richard Fenix <rfenix@Richards-MBP.attlocal.net>
2019-07-08 15:16:11 -0400

    Add lodash and memoize action creator's fetch function.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // for fetching with axios, ensure the 'async' and 'await' commands are included.
@@ -9,13 +10,25 @@ export const fetchPosts = () => async (dispatch) => {
 
 // Note: fetchPosts returns an array of data.
 // for fetchUser, we note that the url returns only 1 object.
-export const fetchUser = (id) => async (dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-  dispatch({type: 'FETCH_USER', payload: response.data})
+// export const fetchUser = (id) => async (dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({type: 'FETCH_USER', payload: response.data})
+// };
+
+// memoizing the fetch function, the new code is shown below:
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
 };
 
-export const selectPost = () => {
-  return {
-    type: 'SELECT_POST'
-  }
-}
+// _ means this is a private function.
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({type: 'FETCH_USER', payload: response.data})
+})
+
+// following code below is just a stub action.
+// export const selectPost = () => {
+//   return {
+//     type: 'SELECT_POST'
+//   }
+// }
